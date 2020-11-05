62d19aa3275bcbe7219b858c289a91813af4fda7
cassymarie <cassy2205@gmail.com>
2020-11-05 12:40:38 -0700

    commit tree 6b748ab7de5042212d517138a98ccf8e63a02e98

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,35 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
 
 def create_project_hash
-  # write your code here
+  # open a file and reads - puts to a variable
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #loops through each instance
+  kickstarter.css("li.project.grid_4").each do |project|
+    #sets the title name
+    title = project.css("h2.bbcard_name strong a").text
+    # creates new hash item with title changed to a symbol
+    projects[title.to_sym] = {
+      # applies values to attributes
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+
+  # return the projects hash
+  projects
+
+  # binding.pry
+  # "let us pry"
+
 end
+
+create_project_hash
